dc46d2aa25f98985c5fd7ab8ca0ec26bbe93317f
Aiman Shahid <aimanshahid800@gmail.com>
2024-08-10 12:57:37 +0500

    Added new pages and updated existing files

---
+++ src/app/homepage/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div className='p-20 bg-red-600 text-black italic'>HomePage</div>
+  )
+}
+
+export default HomePage
---
+++ src/app/namepage/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NamePage() {
+  return (
+    <div className='p-20 bg-violet-700 text-black italic'>NamePage</div>
+  )
+}
+
+export default NamePage
